6c02b1dae6463d9dcc0bb9acc8debf1d713f7403
Lucas Belizario <lucas.belizario375@gmail.com>
2026-06-27 18:37:25 +1000

    feat(inventory_cursor): expand cursor API with new helpers and safer stack handling
    - add get_stack_quantity() for easier and safer quantity access
    - add get_item() to get the cursor's item directly
    - add set_stack_from_values(item, quantity) to construct new stacks from raw values
    - update set_item_stack() to ignore zero or negative quantities and prevent invalid cursor states.
    - maintain compatibility with existing logic

--- scripts/systems/inventory_cursor.gd
+++ scripts/systems/inventory_cursor.gd
@@ -4,21 +4,66 @@ signal content_changed(new_stack: ItemStack)
 
 @export var _stack: ItemStack = null
 
-## Returns the stack.
-func get_item_stack() -> ItemStack:
-	return _stack
-
-## Sets the stack to `value`.
-func set_item_stack(value: ItemStack):
-	_stack = value
-	content_changed.emit(_stack)
-
 ## Clears stack to null.
 func clear():
 	set_item_stack(null)
 
+## Returns the stack.
+func get_item_stack() -> ItemStack:
+	return _stack
+
+## Returns the stack's quantity.
+func get_stack_quantity() -> int:
+	return _stack.quantity if is_holding() else 0
+
+## Returns the stack's item.
+func get_item() -> Item:
+	return _stack.item if is_holding() else null
+
+# Returns how much is left until the stack reaches its max quantity.
+func get_space_left() -> int:
+	if not is_holding():
+		return 0
+	return _stack.item.max_stack - _stack.quantity
+
+## Sets the stack to `value`.
+func set_item_stack(value: ItemStack):
+	if value and value.quantity <= 0:
+		return
+	_stack = value
+	content_changed.emit(_stack)
+
+## Sets the stack from the given values `item` and `quantity`.
+func set_stack_from_values(item: Item, quantity: int):
+	if item and quantity and quantity <= 0:
+		return
+	var new_stack := ItemStack.new(item, quantity)
+	set_item_stack(new_stack)
+
+## Returns True if the cursor is holding a stack
 func is_holding():
 	return true if _stack else false
+
+## Returns True if the cursor can accept that item.
+## Either if the cursor is empty or both have the same item.
+func can_accept(item: Item) -> bool:
+	return not is_holding() or item == _stack.item
+
+## Method to set stack to `quantity`.
+## Returns how much was added (positive) or removed (negative),
+## to achieve the given `quantity`.
+func set_quantity(quantity: int) -> int:
+	if not _stack:
+		return 0
+	
+	var change = quantity - _stack.quantity
+	
+	if change > 0:
+		add(change)
+	if change < 0:
+		subtract(abs(change))
+	
+	return change
 
 ## Adds up to `amount` to the stack.
 ## Returns the leftover that could not fit.
