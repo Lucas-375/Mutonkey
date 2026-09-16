113fb28a78ba4ee59a0075ac4a97856b7db8572b
Lucas Belizario <lucas.belizario375@gmail.com>
2026-09-16 15:13:01 +0300

    feat(player): add component references and class docstring
    - Add reference to MovementComponent and HealthComponent

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,4 +1,17 @@
+## Player entity root.
+## Acts as a container and orchestrator for gameplay systems.
+## Holds references to components and inventory.
+## Delegates all gameplay behaviour to its systems,
+## does not implement logic directly (not like a god-class).
 extends CharacterBody2D
 class_name Player
 
+## Player's inventory resource.
 @export var inventory: Inventory
+
+var movement_component: MovementComponent
+var health_component: HealthComponent
+
+func _ready() -> void:
+    movement_component = $MovementComponent
+    health_component = $HealthComponent
